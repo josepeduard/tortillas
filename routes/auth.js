c84e482089216d08ddbfc24d17ecdbd622bd610a
josepeduard <j.e.garriga.obach@gmail.com>
2019-03-04 15:00:29 +0100

    add login &logout

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,13 @@ const User = require('../models/User');
 
 const saltRounds = 10;
 
+/* Cliente se conecta al servidor si no tiene cookies el servidor genera una session con una cookie y se la da al cliente
+la proxima peticion que hace el cliente, ya passa la cookie y el servidor sabe quien es
+en el momento de hacer el login (POST) el cliente manda la cookie + username + password y el servidor comprueba la cookie, y le assigna una session
+guardando la informacion de la cookie, user y passw */
+
 router.get('/signup', (req, res, next) => {
+  // protegemos la ruta para que si ya estas logueado no puedas acceder al signup
   if (req.session.currentUser) {
     res.redirect('/');
     return;
@@ -15,36 +21,88 @@ router.get('/signup', (req, res, next) => {
 });
 
 router.post('/signup', async (req, res, next) => {
-  // extraer body
+  // no haria falta, pero siempre lo protegemos todo --> protegemos la ruta para que si ya estas logueado no puedas acceder al signup
+  if (req.session.currentUser) {
+    res.redirect('/');
+    return;
+  }
+  // Extraer body --> Comprovar que username y passw no estan vacios
   const { username, password } = req.body;
-  // comprobar que usernam y password existen
   if (!password || !username) {
     res.redirect('/auth/signup');
+    return;
   }
-  // comprobar que el usuario no existe
+  // --> comprovar que el usuario no existe
   try {
-    const result = await User.findOne({ username });
+    const result = await User.findOne({ username: username });
     if (result) {
       res.redirect('/auth/signup');
       return;
     }
-    // encriptar la contraseña (password)
+    // --> encriptar passw --> Crear el usuario
     const salt = bcrypt.genSaltSync(saltRounds);
     const hashedPassword = bcrypt.hashSync(password, salt);
-    // crear usuario
     const newUser = {
       username,
       password: hashedPassword
     };
-
-    const createdUser = await User.create(newUser);
+    const createUser = await User.create(newUser);
     // Guardamos el usuario en la session
-    req.session.currentUser = createdUser;
-    // Redirigimos para la homepage
+    req.session.currentUser = createUser;
     res.redirect('/');
   } catch (error) {
     next(error);
   }
 });
 
+router.get('/login', (req, res, next) => {
+  if (req.session.currentUser) {
+    res.redirect('/');
+    return;
+  }
+  res.render('auth/login');
+});
+
+router.post('/login', async (req, res, next) => {
+  // proteccion si estamos logeados no podemos acceder a login
+  if (req.session.currentUser) {
+    res.redirect('/');
+    return;
+  }
+  // Extraer informacion del body
+  const { username, password } = req.body;
+  // Comprobar que hay usuario y password
+  if (!password || !username) {
+    res.redirect('/auth/login');
+    return;
+  }
+  // Comprovar que el usuario existe
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      res.redirect('/auth/login');
+      return;
+    }
+    // Comparar la contraseña
+    if (bcrypt.compareSync(password, user.password)) {
+      // Guardar la sesion
+      req.session.currentUser = user;
+      // Redirigir
+      res.redirect('/');
+    } else {
+      res.redirect('/auth/login');
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/logout', (req, res, next) => {
+  if (!req.session.currentUser) {
+    res.redirect('/');
+    return;
+  }
+  delete req.session.currentUser;
+  res.redirect('/');
+});
 module.exports = router;
